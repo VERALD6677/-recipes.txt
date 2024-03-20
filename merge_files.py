9b2db02718eb219d3a1356f0d28c0d749955728d
VERALD6677 <44982435+VERALD6677@users.noreply.github.com>
2024-03-20 12:57:43 +0300

    Update merge_files.py

--- merge_files.py
+++ merge_files.py
@@ -1,19 +1,26 @@
 #Zad#3
 import os
 
-def merge_files(files):
-  """
-  Объединяет несколько файлов в один по правилам, описанным в задаче.
-  """
-  result_file = open('merged_files.txt', 'w')
-  for file in files:
-    with open(file, 'r') as f:
-      num_lines = len(f.readlines())
-    result_file.write(f'{file}\n{num_lines}\n')
-    with open(file, 'r') as f:
-      result_file.write(f.read())
-    result_file.write('\n')
-  result_file.close()
+file_contents = {}
 
-files = ['1.txt', '2.txt', '3.txt']
-merge_files(files)
+# Читаем содержимое файлов и сохраняем количество строк
+for filename in os.listdir('.'):
+    if filename.endswith('.txt'):
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            file_contents[filename] = {
+                'num_lines': len(lines),
+                'content': lines
+            }
+
+# Сортируем файлы по количеству строк
+sorted_files = sorted(file_contents.items(), key=lambda x: x[1]['num_lines'])
+
+# Записываем отсортированное содержимое в новый файл
+with open('result.txt', 'w') as result_file:
+    for file_name, content in sorted_files:
+        result_file.write(f"{file_name}\n{content['num_lines']}\n")
+        result_file.writelines(content['content'])
+        result_file.write('\n\n')
+
+print("Готово! 'result.txt'")
